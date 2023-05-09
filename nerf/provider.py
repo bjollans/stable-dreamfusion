6645a551a1826a941530c1aa81ec74b8e90da781
Bernard Jollans <jollans94@googlemail.com>
2023-05-09 09:41:20 +0200

    auto-commiting for fast experimentation -> no good msg

--- nerf/provider.py
+++ nerf/provider.py
@@ -150,8 +150,8 @@ def rand_poses(size, device, radius_range=[1, 1.5], theta_range=[0, 120], phi_ra
 # TODO make the angle_front 0 and then 180 and see what happens
 # TODO make a pull request that makes the default values as torch.FloatTensor([self.opt.default_phi]).to(self.device)
 def circle_poses(device, radius=1.25, theta=60, phi=0, return_dirs=False, angle_overhead=30, angle_front=60):
-    angle_front = phi=torch.FloatTensor([180]).to(device)
-    #phi=torch.FloatTensor([180]).to(device)
+    #angle_front = phi=torch.FloatTensor([0]).to(device)
+    phi=torch.FloatTensor([180]).to(device)
 
     theta = theta / 180 * np.pi
     phi = phi / 180 * np.pi
