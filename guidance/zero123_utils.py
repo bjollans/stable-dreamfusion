fccadee878755ce4ff21efe4a1fddc6df3754866
Bernard Jollans <jollans94@googlemail.com>
2023-05-24 14:48:11 +0200

    just testing stuff

--- guidance/zero123_utils.py
+++ guidance/zero123_utils.py
@@ -164,7 +164,10 @@ class Zero123(nn.Module):
         for img in imgs:
             # save as random file name with date in it .png
             torchvision.transforms.ToPILImage()(img).save(f'./latents_{datetime.datetime.now().strftime("%Y%m%d_%H%M%S%f")}_{uuid.uuid4().hex}.png')
-            torchvision.transforms.ToPILImage()(clip_emb).save(f'./cond_{datetime.datetime.now().strftime("%Y%m%d_%H%M%S%f")}_{uuid.uuid4().hex}.png')
+        imgs = self.decode_latents(clip_emb)
+        for img in imgs:
+            # save as random file name with date in it .png
+            torchvision.transforms.ToPILImage()(img).save(f'./cond_{datetime.datetime.now().strftime("%Y%m%d_%H%M%S%f")}_{uuid.uuid4().hex}.png')
         # print(polar, azimuth, radius)
         # kiui.vis.plot_image(pred_rgb_256, imgs)
 
